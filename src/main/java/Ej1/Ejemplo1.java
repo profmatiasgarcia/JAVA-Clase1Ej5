0c55c1ad9ddd5dc4bcc4c571d2e8dee61166b09c
Prof Matias Garcia <54009386+profmatiasgarcia@users.noreply.github.com>
2021-02-18 19:38:42 -0300

    Add files via upload

---
+++ src/main/java/Ej1/Ejemplo1.java
@@ -0,0 +1,13 @@
+package Ej1;
+/**
+ * En un hospital existen tres área : ginecología, pediatría y traumatología
+ * El  presupuesto Anual del hospital se reparte de la siguiente manera:
+ * 1.Ginecología        40%
+ * 2.Pediatría          30%
+ * 3.Traumatología      25%
+ * 4.Administración      5%
+ * Determinar cuanta cantidad de dinero va a cada area siendo el monto de presupuesto el dato de entrada
+ */
+public class Ejemplo1 {
+    
+}
