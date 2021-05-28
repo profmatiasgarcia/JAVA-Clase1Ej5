877ec6a36b5f6c906ca40632bc5cde4618a0faac
Prof Matias Garcia <54009386+profmatiasgarcia@users.noreply.github.com>
2021-05-28 19:32:22 -0300

    Add files via upload

---
+++ src/main/java/Ej3/Ejemplo3.java
@@ -0,0 +1,14 @@
+package Ej3;
+/**
+ * Se registran las notas de los parciales de los 15 estudiantes de la materia Programacion 3 y se desea visualizar
+ * 1) El promedio total
+ * 2) La mayor nota y la menor nota
+ * 3) Cuantos estudiantes aprobaron (nota mayor igual a 4) y cuantos deberan recuperar el examen
+ * 4) Cuantos estudiantes sacaron notas entre 8 y 10, cual es el promedio con respecto al total de examinados
+ * 
+ * Que modificaciones habria que hacer para registrar la nota del 1er parcial, la nota de recuperatorio si requiriera,
+ * la nota del 2do parcial con su recuperatorio, calcular la nota de cursada y registrar la nota de final
+ */
+public class Ejemplo3 {
+
+}
